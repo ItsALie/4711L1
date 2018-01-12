8389e1d6995480c6b342a28829a6255bbb9f5614
Haley Booker <haley1652@hotmail.com>
2018-01-12 10:33:29 -0800

    Added comments

--- Student.php
+++ Student.php
@@ -8,6 +8,7 @@
 
 /**
  * Description of Student
+ * A basic student class with minimum functionality
  *
  * @author Haley
  */
@@ -20,14 +21,17 @@ class Student {
         $this->grades = array();
     }
     
+    //Adds email to the student
     function add_email($which,$address) {
         $this->emails[$which] = $address;
     }
-
+    
+    //Adds a grade to the student
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
     
+    //Takes all student grades and returns the average
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -35,6 +39,8 @@ class Student {
         return $total / count($this->grades);
     }
     
+    /*Displays the first name, last name, grade average and all
+    emails associated with the student*/
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('.$this->average().")\n";
